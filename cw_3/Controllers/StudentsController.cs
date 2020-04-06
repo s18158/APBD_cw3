df6b5322ce6267ac8a033f0932c643f84c178006
s18158 <s18158@pjwstk.edu.pl>
2020-04-06 18:53:50 +0200

    zad6

--- cw_3/Controllers/StudentsController.cs
+++ cw_3/Controllers/StudentsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using cw_3.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace cw_3.Controllers
@@ -10,10 +11,26 @@ namespace cw_3.Controllers
     [Route("api/students")]
     public class StudentsController : ControllerBase
     {
-        [HttpGet]
-        public string GetStudent(string orderBy)
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
         {
-            return $"Kowalski, Malewski, Andrzejewski sortowanie={orderBy}";
+            if (id == 1)
+            {
+                return Ok("Kowalski");
+            }
+            else if (id == 2)
+            {
+                return Ok("Malewski");
+            }
+
+            return NotFound("Nie znaleziono studenta");
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
         }
     }
 }
